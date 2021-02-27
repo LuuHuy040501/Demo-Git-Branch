32eaebb011d7efb73d27222d4852d9c1747dd6f3
huyldth1904081 <huyldth1904081@fpt.edu.vn>
2021-02-27 09:24:01 +0700

    init project

---
+++ DemoGitBranch/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoGitBranch.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //implement login logout function
+    }
+}
